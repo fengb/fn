0e3400a7d96d466ad464ad30c754198282811e27
fengb <none@none>
2008-12-02 20:32:06 -0600

    Changing setup.py to install fn_rest

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 
 from distutils.core import setup
 setup(
-    name='fn_blog',
-    description='fn_blog is a Django app',
+    name='fn',
+    description='fn is a Django web suite',
     author='Benjamin Feng',
     license='3-clause BSD',
+    py_modules=['fn_rest'],
     packages=['fn_blog'],
     package_data={'fn_blog': ['templates/base.html', 'templates/fn_blog/*.html']},
 )
