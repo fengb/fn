e6e24947a2352bd184b5977e586d1b5492cca9b5
fengb <none@none>
2009-02-27 23:59:52 -0600

    Adding wildcard support for URL matching

--- fn_style/context_processors.py
+++ fn_style/context_processors.py
@@ -1,4 +1,4 @@
 from . import models
 
 def fn_styles(request):
-    return {'fn_styles': models.Style.for_url(request.path)}
+    return {'fn_styles': models.Url.styles(request.path)}
--- fn_style/models.py
+++ fn_style/models.py
@@ -8,11 +8,20 @@ class Style(Model):
     owner = ForeignKey(User)
     body = TextField()
 
-    @classmethod
-    def for_url(cls, url):
-        return [url.style for url in Url.objects.filter(match=url)]
+    def __str__(self):
+        return str(self.id)
 
 
 class Url(Model):
     style = ForeignKey(Style)
     match = CharField(max_length=100)
+
+    def __str__(self):
+        return self.match
+
+    @classmethod
+    def styles(cls, path):
+        # TODO: Make this structure less confusing
+        # This isn't reversed. We need to find all entries that matches.
+        urls = Url.objects.extra(where=['%s LIKE match'], params=[path])
+        return [url.style for url in urls]
