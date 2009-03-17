1a7a94d56e09d654f2688179245c8f5363853c49
fengb <none@none>
2009-03-16 20:11:57 -0500

    Switching back to DateField for now

--- fn_blog/models.py
+++ fn_blog/models.py
@@ -1,13 +1,8 @@
+from django.db.models import Model
 from django.db.models import ForeignKey, ManyToManyField
 from django.db.models import CharField, TextField, DateField, BooleanField
 from django.contrib.auth.models import User
->>>>>>> other
-
-<<<<<<< local
-from django.contrib.auth.models import User
 from fn_category.models import Category
-=======
->>>>>>> other
 
 
 class Blog(Model):
@@ -24,7 +19,7 @@ class Entry(Model):
     body = TextField()
     public = BooleanField()
     categories = ManyToManyField(Category)
-    created = DateTimeField(auto_now_add=True)
+    created = DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
