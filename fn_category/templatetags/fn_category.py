237bd4736f626e4732e4c6bf2055e32b2d1e27d7
fengb <none@none>
2009-04-29 01:36:28 -0500

    Moved tag back into category

--- fn_category/templatetags/fn_category.py
+++ fn_category/templatetags/fn_category.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.safestring import mark_safe
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
@@ -6,3 +8,19 @@ register = template.Library()
 @register.filter
 def contains(value, arg):
     return arg in value
+
+
+@register.filter
+def link(value, arg):
+    """Naive link tag. Requires value to be a partial.
+
+    Example: func_var|link:1
+    """
+    view = '%s.%s' % (value.func.__module__, value.func.__name__)
+    if value.keywords:
+        kwargs = {'category_id': arg}
+        kwargs.update(value.keywords)
+        return mark_safe(reverse(view, kwargs=kwargs))
+    else:
+        args = value.args + (arg,)
+        return mark_safe(reverse(view, args=args))
