f967426b433a61f6da72ba3adbd981e55f258680
fengb <none@none>
2009-04-29 19:36:21 -0500

    Changed variable population to use locals() as it reads better

--- fn_blog/views.py
+++ fn_blog/views.py
@@ -6,43 +6,37 @@ from . import models
 
 
 def blog_list(request):
-    vars = {}
-    vars['blogs'] = models.Blog.objects.all()
+    blogs = models.Blog.objects.all()
 
-    return render_to_response('fn_blog/blog_list.html', vars,
+    return render_to_response('fn_blog/blog_list.html', locals(),
                               context_instance=RequestContext(request))
 
 
 def blog(request, id):
-    vars = {}
-    vars['blog'] = blog = models.Blog.objects.get(id=int(id))
-    vars['entries'] = blog.entries_by_user(request.user)
-    vars['fn_category_view'] = partial(blog_category, blog.id)
+    blog = models.Blog.objects.get(id=int(id))
+    entries = blog.entries_by_user(request.user)
+    fn_category_view = partial(blog_category, blog.id)
 
-    return render_to_response('fn_blog/blog.html', vars,
+    return render_to_response('fn_blog/blog.html', locals(),
                               context_instance=RequestContext(request))
 
 
 def blog_category(request, id, category_id):
-    vars = {}
-    vars['blog'] = blog = models.Blog.objects.get(id=int(id))
-    vars['fn_category_view'] = partial(blog_category, blog.id)
-
+    blog = models.Blog.objects.get(id=int(id))
     category = models.Category.objects.get(id=int(category_id))
-    vars['entries'] = blog.entries_by_user(request.user).filter(categories=category)
-    vars['fn_category_selected'] = [category]
+    entries = blog.entries_by_user(request.user).filter(categories=category)
+    fn_category_view = partial(blog_category, blog.id)
+    fn_category_selected = [category]
 
-    return render_to_response('fn_blog/blog.html', vars,
+    return render_to_response('fn_blog/blog.html', locals(),
                               context_instance=RequestContext(request))
 
 
 def entry(request, id):
     entry = models.Entry.objects.get(id=int(id))
+    blog = entry.blog
+    fn_category_view = partial(blog_category, blog.id)
+    fn_category_selected = set(entry.categories.iterator())
 
-    vars = {}
-    vars['entry'] = entry
-    vars['blog'] = blog = entry.blog
-    vars['fn_category_view'] = partial(blog_category, blog.id)
-    vars['fn_category_selected'] = set(entry.categories.iterator())
-    return render_to_response('fn_blog/entry.html', vars,
+    return render_to_response('fn_blog/entry.html', locals(),
                               context_instance=RequestContext(request))
