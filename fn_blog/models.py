a6cb3d8f4ac51004f4be598458cb78338fbfac49
fengb <none@none>
2009-05-01 00:42:13 -0500

    Added URL resolution to blog models

--- fn_blog/models.py
+++ fn_blog/models.py
@@ -1,16 +1,18 @@
-from django.db.models import Model
-from django.db.models import ForeignKey, ManyToManyField
-from django.db.models import CharField, TextField, DateField, BooleanField
+from django.db import models
 from django.contrib.auth.models import User
 from fn_category.models import Category
 
 
-class Blog(Model):
-    owner = ForeignKey(User)
-    name = CharField(max_length=100)
+class Blog(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'fn_blog.views.blog', str(self.id)
 
     def entries_by_user(self, user):
         if self.owner == user:
@@ -19,13 +21,21 @@ class Blog(Model):
             return self.entry_set.filter(public=True).order_by('-created')
 
 
-class Entry(Model):
-    blog = ForeignKey(Blog)
-    title = CharField(max_length=100)
-    body = TextField()
-    public = BooleanField()
-    categories = ManyToManyField(Category)
-    created = DateField(auto_now_add=True)
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    public = models.BooleanField()
+    categories = models.ManyToManyField(Category)
+    created = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'fn_blog.views.entry', str(self.id)
+
+    @property
+    def owner(self):
+        return self.blog.owner
