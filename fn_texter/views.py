cd0a4211325b07ad1f7f8d3ead2502bc6342bcf6
fengb <none@none>
2010-01-23 12:25:26 -0800

    Extended external API to allow color.  Added resizing for anti alias.

--- fn_texter/views.py
+++ fn_texter/views.py
@@ -5,17 +5,21 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 
-def texter(request, font_face, font_size, text):
+SCALE = 2
+
+
+def texter(request, font_face, font_size, color, text):
     font_file = os.path.join(settings.FN_TEXTER_DIR, font_face)
     try:
-        font = ImageFont.truetype(font_file, int(font_size))
+        font = ImageFont.truetype(font_file, int(font_size) * SCALE)
     except IOError:
         raise Http404
     else:
         size = font.getsize(text)
         image = Image.new('RGBA', size)
         draw = ImageDraw.Draw(image)
-        draw.text((0, 0), text, font=font, fill='#000')
+        draw.text((0, 0), text, font=font, fill='#'+color)
+        image.thumbnail([coord / SCALE for coord in size], Image.ANTIALIAS)
         response = HttpResponse(mimetype='image/png')
         image.save(response, 'PNG')
         return response
