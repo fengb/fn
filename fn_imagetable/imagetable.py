a6f414cd4bb0e881e89a88864439e867b5cee4b9
fengb <none@none>
2011-02-17 20:13:01 -0600

    Updated algorithm to use streams for lighter memory usage.

--- fn_imagetable/imagetable.py
+++ fn_imagetable/imagetable.py
@@ -10,23 +10,30 @@ class ImageTable(object):
     def from_file(cls, file, *args, **kwargs):
         return cls(Image.open(file).convert('RGB'), *args, **kwargs)
 
-    def styles(self):
-        return '''
-table.%s{border-collapse:collapse;border-spacing:0;width:auto;height:auto}
-table.%s td{width:1px;height:1px;padding:0}
-        ''' % (self.c, self.c)
-
-    def table(self):
-        rows = []
-        for y in range(self.image.size[1]):
-            rows.append(''.join('<td style="background:%s"></td>' % self.hexvalue(x, y) for x in range(self.image.size[0])))
-        return '<table class="%s"><tr>' % self.c + '</tr>\n<tr>'.join(rows) + '</tr></table>'
-
-    def html(self):
-        return '<html><head><style>%s</style><body>%s</body></html>' % (self.styles(), self.table())
-
     def hexvalue(self, x, y):
         return '#%02x%02x%02x' % self.pixels[x, y]
+
+    def styles(self, fout):
+        fout.write('''
+table.%s{border-collapse:collapse;border-spacing:0;width:auto;height:auto}
+table.%s td{width:1px;height:1px;padding:0}
+        ''' % (self.c, self.c))
+
+    def table(self, fout):
+        fout.write('<table class="%s">' % self.c)
+        for y in range(self.image.size[1]):
+            fout.write('<tr>')
+            for x in range(self.image.size[0]):
+                fout.write('<td style="background:%s"></td>' % self.hexvalue(x, y))
+            fout.write('</tr>')
+        fout.write('</table>')
+
+    def html(self, fout):
+        fout.write('<html><head><style>')
+        self.styles(fout)
+        fout.write('</style><body>')
+        self.table(fout)
+        fout.write('</body></html>')
 
 
 from_file = ImageTable.from_file
@@ -35,4 +42,4 @@ from_file = ImageTable.from_file
 if __name__ == '__main__':
     import sys
     it = ImageTable.from_file(sys.argv[1])
-    print it.html()
+    it.html(sys.stdout)
