f3a0ed7ae0c0316e0c3da8891a5a8a396e8ca9c1
fengb <none@none>
2011-02-17 19:03:26 -0800

    Fixed fn_imagetable to use streamed output.

--- fn_imagetable/views.py
+++ fn_imagetable/views.py
@@ -1,6 +1,7 @@
 import imagetable
+import tempfile
+from django import forms, http
 from django.shortcuts import render_to_response
-from django import forms
 
 
 class UploadFileForm(forms.Form):
@@ -11,7 +12,11 @@ def upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            return render_to_response('fn_imagetable/render.html', {'imagetable': imagetable.from_file(request.FILES['file'])})
+            f = tempfile.TemporaryFile()
+            it = imagetable.from_file(request.FILES['file'])
+            it.html(f)
+	    f.seek(0)
+            return http.HttpResponse(f.read())
     else:
         form = UploadFileForm()
 
